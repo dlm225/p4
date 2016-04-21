822ebdf56880622441392e458b11deb39d917bc6
David Morgan <dlm225@g.harvard.edu>
2016-04-21 09:43:58 -0400

    small css change to leaderboard

--- resources/views/more/leaderboard.blade.php
+++ resources/views/more/leaderboard.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-    <div class='container'>
+    <div class='container leader-container'>
         <div class='row'>
             <div class='col-md-6 col-md-offset-3 centerbox'>
                 <img src='/images/trophy.png' width='75px' height='75px'>
@@ -14,15 +14,18 @@
             </div>
         </div>
         <div class='row'>
-            <div class='col-md-6 col-md-offset-3 shadowbox'>
+            <div class='col-md-8 col-md-offset-2 shadowbox'>
+                <span class='leader-text'> 1 </span>
                 <img class='leader-pic' src='/images/defaultprofile.png' width='50px' height='50px'>
                 <span class='leader-text'> Username 1</span>
                 <span class='leader-text'> Points: 2342342</span>
                 <span class='leader-text'> Last Login: 42 days ago</span>
             </div>
         </div>
+        <br />
         <div class='row'>
-            <div class='col-md-6 col-md-offset-3 shadowbox'>
+            <div class='col-md-8 col-md-offset-2 shadowbox'>
+                <span class='leader-text'> 2 </span>
                 <img class='leader-pic' src='/images/defaultprofile.png' width='50px' height='50px'>
                 <span class='leader-text'> Username 2</span>
                 <span class='leader-text'> Points: 2342342</span>
