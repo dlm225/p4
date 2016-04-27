af59d8255b2f93df93c2f4fa30d74f731396be1a
David Morgan <dlm225@g.harvard.edu>
2016-04-27 14:56:47 -0400

    update to ProfileController to handle post changes

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -21,8 +21,27 @@ class ProfileController extends Controller {
         return view('profile.profileupdate')->with('user',$user);
     }
 
-    public function postProfileUpdate() {
+    public function postProfileUpdate(Request $request) {
+        $book = \App\Book::find($request->id);
+
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        $book->published = $request->published;
+        $book->cover = $request->cover;
+        $book->purchase_link = $request->purchase_link;
+        if($request->tags) {
+            $tags = $request->tags;
+        }
+        else {
+            $tags = [];
+        }
+        $book->tags()->sync($tags);
+        $book->save();
+
+        \Session::flash('message','Your changes were made.');
+        return redirect('/book/edit/'.$request->id);
+    }
+
         $user = Auth::user();
         return view('profile.profileupdate')->with('user',$user);
-    }
 }
