3ef3f89e9a48f62473a5d1a106ffe0f83496846e
David Morgan <dlm225@g.harvard.edu>
2016-05-02 08:37:03 -0400

    fixed login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,7 @@
 
                     <div class='form-group'>
                         <label for='email'>Email</label>
-                        <input type='text' name='email' id='email' value='{{ old('email') }} '>
+                        <input type='text' name='email' id='email' value='{{ old('email') }}'>
                     </div>
 
                     <div class='form-group'>
