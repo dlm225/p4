a3829e5942cde6261b858fdb4242b6e08a09d2ff
David Morgan <dlm225@g.harvard.edu>
2016-05-12 15:35:24 -0400

    more validation fixes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,14 +14,14 @@
 <h3>Credits</h3>
 
 <h4>Login & Registation Popup</h4>
-    <ul>Bijay Pakhrin @ AndWeCode -- http://andwecode.com/create-popup-login-and-signup-form</ul>
+    <ul><li>Bijay Pakhrin @ AndWeCode -- http://andwecode.com/create-popup-login-and-signup-form</li></ul>
 
 <h4>Trophy Icon</h4>
-    <ul>FlashWOD -- https://www.flashwod.com/img/home-images/score-icon.png</ul>
+    <ul><li>FlashWOD -- https://www.flashwod.com/img/home-images/score-icon.png</li></ul>
 
 <h4>Tablesorter</h4>
-    <ul>Christian Bach -- http://tablesorter.com/docs/ (MIT Open License)</ul>
+    <ul><li>Christian Bach -- http://tablesorter.com/docs/ (MIT Open License)</li></ul>
 
 <h4>Table CSS</h4>
-    <ul>Jake Rocheleau -- http://codepen.io/jakestuts/</ul>
+    <ul><li>Jake Rocheleau -- http://codepen.io/jakestuts/</li></ul>
 @stop
